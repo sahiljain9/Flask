d93748207cfc918bad0e3f51ff746f598daf5870
Sahil Sanjay Jain <103605963+sahiljain9@users.noreply.github.com>
2026-03-20 20:58:21 +0530

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/")#URL leading to method
+def hello(): # Name of the method
+ return("Hello World!")
+@app.route("/greetme")#different URL
+def helloall(): # different method name
+ name = request.args.get('name')#retrieve GET parameters
+ return("Hello {}!".format(name))#Python’s string.format
 if __name__ == "__main__":
-    app.run(
-        host='0.0.0.0',
-        port=8080,
-        ssl_context=('cert.pem', 'privkey.pem') )
+ app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080 - Delete SSL context if not secured with domain
